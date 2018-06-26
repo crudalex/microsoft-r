122ae26d5342869ec50f0663b265b992fd570f65
atwlam <crudalex@gmail.com>
2018-06-27 01:43:21 +0800

    Merge branch 'master' of github.com:crudalex/microsoft-r
    Signed-off-by: atwlam <crudalex@gmail.com>

--- scraper/get_race_html.py
+++ scraper/get_race_html.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import time
 
 import requests
+
 url = 'http://racing.hkjc.com/racing/info/meeting/Results/English/'
 html = requests.get(url).text
 
 from lxml import etree
+
 selector = etree.HTML(html)
 
 base_url = 'http://racing.hkjc.com/racing/info/meeting/Results/English/'
@@ -21,10 +24,32 @@ datadir = os.path.join(workdir, 'data', 'races')
 if not os.path.exists(datadir):
     os.mkdir(datadir)
 
-for url in race_urls:
+start_time = time.time()
+for u in race_urls:
     html = requests.get(url).text
-
     filename = re.sub('\W+', '_', url.rstrip().lower()) + '.html'
     outfile = os.path.join(datadir, filename)
     with open(outfile, 'w', encoding='utf8') as out:
         out.write(html)
+end_time = time.time()
+print("Single threaded time: %s" % str(end_time - start_time))
+
+
+def grab_url(url):
+    html = requests.get(url).text
+    filename = re.sub('\W+', '_', url.rstrip().lower()) + '.html'
+    outfile = os.path.join(datadir, filename)
+    with open(outfile, 'w', encoding='utf8') as out:
+        out.write(html)
+
+
+from multiprocessing.dummy import Pool as ThreadPool
+#from multiprocessing import Pool as ThreadPool
+
+pool = ThreadPool(8)
+start_time = time.time()
+pool.map(grab_url, race_urls)
+pool.close()
+pool.join()
+end_time = time.time()
+print("Multi threaded time: %s" % str(end_time - start_time))
